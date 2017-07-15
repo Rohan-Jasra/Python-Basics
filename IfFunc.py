3e4a1a0f066b24b473ab7f62b9419a403b4871bd
RohanJas <rohan.jasra@gmail.com>
2017-07-14 21:00:12 -0400

    Add files via upload

---
+++ IfFunc.py
@@ -0,0 +1,16 @@
+def Gym(x):
+    print ("Hello friend, you have just entered the gym!")
+    print ("Do you plan to train like a Bodybuilder or a Powerlifter?")
+
+    if x=="Powerlifter" or x=="Powerlifter".lower():
+        return 'Great! Going heavy with with paramters like 3x3 or 3x5 will yield you the results you are looking for, explosiveness is the aim.'
+    elif x=="Bodybuilder" or x=="Bodybuilder".lower():
+        return 'Excellent! 3 sets of 10 is a good parameter to follow and controlling the weight is key'
+    else:
+        return "Well...there'/s always power building!"
+    
+x="powerlifter"
+y="bodybuilder"
+
+
+"""Just practicing functions with some conditional statements."""
